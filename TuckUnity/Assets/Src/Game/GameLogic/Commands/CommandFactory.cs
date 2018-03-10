8d0c3f15707445222484f5ccca1a8dfbb6032f4a
Julian Williams <jdfrag@gmail.com>
2018-03-10 01:04:57 -0800

    Add Redo functionality & remove ability to link commands

--- TuckUnity/Assets/Src/Game/GameLogic/Commands/CommandFactory.cs
+++ TuckUnity/Assets/Src/Game/GameLogic/Commands/CommandFactory.cs
@@ -5,36 +5,53 @@ using UnityEngine;
 public class CommandFactory
 {
     private Stack<ICommand> _undoStack = new Stack<ICommand>();
+    private Stack<ICommand> _redoStack = new Stack<ICommand>();
 
     public void Execute(ICommand command)
     {
         Debug.Log("Executing command: " + command.GetType().ToString());
         command.Execute();
         _undoStack.Push(command);
+        _redoStack.Clear();
     }
    
+    public bool Redo()
+    {
+        if (_redoStack.Count == 0)
+        {
+            Debug.Log("Redo Command Stack is empty!");
+            return false;
+        }
+
+        ICommand command = _redoStack.Pop();
+        Debug.Log("Redoing command: " + command.GetType().ToString());
+
+        command.Execute();
+        _undoStack.Push(command);
+        
+        return true;
+    }
+
     public bool Undo()
     {
         if(_undoStack.Count == 0)
         {
-            Debug.Log("Command Stack is empty!");
+            Debug.Log("Undo Command Stack is empty!");
             return false;
         }
+        
+        ICommand command = _undoStack.Pop();
+        Debug.Log("Undoing command: " + command.GetType().ToString());
 
-        ICommand command;
-        do
-        {
-            command = _undoStack.Pop();
-            Debug.Log("Undoing command: " + command.GetType().ToString());
-            command.Undo();
-        }
-        while (command.isLinked);
-        //Maybe save this command into another stack for re-do's
+        command.Undo();
+        _redoStack.Push(command);
+
         return true;
     }
 
     public void Clear()
     {
         _undoStack.Clear();
+        _redoStack.Clear();
     }
 }
